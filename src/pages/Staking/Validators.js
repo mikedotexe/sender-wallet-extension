73570868f4f8110ced114dd5a005aab9533e5453
AmazingBeerBelly <amazingbeerbelly@gmail.com>
2021-10-22 18:57:49 +0800

    add unstake select validator

--- src/pages/Staking/Validators.js
+++ src/pages/Staking/Validators.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
-import { useHistory } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import Avatar from '@material-ui/core/Avatar';
 
 import styled from 'styled-components';
 
@@ -15,7 +14,6 @@ import Input from '../../components/Input';
 import List from '../../components/List';
 import backIcon from '../../assets/img/back.png';
 import searchIcon from '../../assets/img/search.png';
-import selectedIcon from '../../assets/img/selected.png';
 
 const WrapperBasePage = styled(BasePage)`
   .search-input {
@@ -64,8 +62,29 @@ const ValidatorItem = ({ data: validator }) => {
   )
 }
 
+const UnstakeValidatorItem = ({ data: validator }) => {
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '100%', paddingLeft: '25px', paddingRight: '25px', height: '75px', boxSizing: 'border-box' }}>
+      <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+        <Typography sx={{ fontSize: '14px', color: 'white', lineHeight: '24px' }}>{validator.accountId}</Typography>
+        <Typography sx={{ fontSize: '14px', color: '#FAD165', fontWeight: 'bold' }}>{validator.amount} NEAR</Typography>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <Typography sx={{ fontSize: '14px', color: '#777777', fontWeight: 'bold' }}>{validator.fee}%Fee</Typography>
+          <Typography sx={{ fontSize: '14px', color: '#777777', marginLeft: '4px', marginRight: '5px' }}>-</Typography>
+          <Typography sx={{ fontSize: '14px', color: '#777777', fontWeight: 'bold' }}>{validator.active ? 'active' : 'inactive'}</Typography>
+        </Box>
+      </Box>
+
+      <Button sx={{ backgroundColor: '#343434', border: '1px solid #4D4D4D', boxSizing: 'border-box', borderRadius: '13px' }}>
+        <Typography sx={{ fontSize: '14px', color: '#777777', lineHeight: '10px' }}>Select</Typography>
+      </Button>
+    </Box>
+  )
+}
+
 const Validators = () => {
   const history = useHistory();
+  const { isUnstake } = useParams();
 
   const backClicked = () => {
     history.goBack();
@@ -78,16 +97,26 @@ const Validators = () => {
           <img src={backIcon} alt='back'></img>
         </Button>
 
-        <Typography sx={{ fontSize: '16px', color: '#777777' }}>Select Assets</Typography>
+        <Typography sx={{ fontSize: '16px', color: '#777777' }}>Select a Validator</Typography>
       </Box>
 
-      <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: '25px', flexDirection: 'column' }}>
-        <BaseBox sx={{ justifyContent: 'start' }}>
-          <img src={searchIcon} alt="search"></img>
-          <Input className='search-input' placeholder='Validator account ID'></Input>
-        </BaseBox>
+      {
+        isUnstake && (
+          <Typography sx={{ fontSize: '14px', color: '#777777', marginTop: '15px' }} align='center'>Select the validator pool from which you wish to unstake your tokens</Typography>
+        )
+      }
 
-        <List list={validators} Component={ValidatorItem} sx={{ width: '100%', marginTop: '15px', marginBottom: '15px' }}>
+      <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: '25px', flexDirection: 'column' }}>
+        {
+          !isUnstake && (
+            <BaseBox sx={{ justifyContent: 'start' }}>
+              <img src={searchIcon} alt="search"></img>
+              <Input className='search-input' placeholder='Validator account ID'></Input>
+            </BaseBox>
+          )
+        }
+
+        <List list={validators} Component={isUnstake ? UnstakeValidatorItem : ValidatorItem} sx={{ width: '100%', marginTop: '15px', marginBottom: '15px' }}>
         </List>
       </Box>
     </WrapperBasePage>
