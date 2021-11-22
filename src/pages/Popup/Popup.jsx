53238c26bfd99f20efc24b5172486273a5a4d5ed
AmazingBeerBelly <amazingbeerbelly@gmail.com>
2021-11-22 11:01:25 +0800

    add ga config

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -3,12 +3,15 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { ConnectedRouter } from 'connected-react-router';
 import { ThemeProvider } from '@material-ui/core/styles';
+import ReactGA from 'react-ga';
 
 import './Popup.css';
 import theme from '../../theme';
 
 import Router from '../../Router';
 import { store, persistor, history } from '../../Store';
+
+ReactGA.initialize('G-VF50EXY360');
 
 const Popup = () => {
   return (
